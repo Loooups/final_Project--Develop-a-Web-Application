3b2d59fbbe794ac9830ad37a3b37ac7e866f1700
fmoncef <114942078+fmoncef@users.noreply.github.com>
2022-11-29 15:00:22 +0100

    set async all studies, patients and physicians CRUD

--- backend/controllers/patientsController.js
+++ backend/controllers/patientsController.js
@@ -1,32 +1,33 @@
+const asyncHandler = require("express-async-handler");
 const studies = { idStudy: "098cvb456", studyDateStart: "12/04/2021" };
 
 //@desc Get all patients
 //@route GET /api/patients
 //@access Private
-const getPatients = (req, res) => {
+const getPatients = asyncHandler(async (req, res) => {
   res.status(200).json(studies);
-};
+});
 
 //@desc add patient
 //@route POST /api/patients
 //@access Private
-const setPatient = (req, res) => {
+const setPatient = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Patient added " });
-};
+});
 
 // @desc update patient
 // @route PUT /api/patients
 // @access Private
-const updatePatient = (req, res) => {
+const updatePatient = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Patient number: ${req.params.id}` });
-};
+});
 
 // @desc delete patient
 // @route DELETE /api/patients
 // @access Private
-const deletePatient = (req, res) => {
+const deletePatient = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Patient number ${req.params.id}` });
-};
+});
 
 module.exports = {
   getPatients,
