9f3873015179c9645b1f957be36a9df9db19efae
fmoncef <114942078+fmoncef@users.noreply.github.com>
2022-12-13 17:23:42 +0100

    test correction docs front react for githubactions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,8 +6,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    {/* <React.StrictMode> */}
-    <App />
-    {/* </React.StrictMode> */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </div>
 );
